d0a5729b3d1afaf31c0efd5a8a67755b0b0c9f86
Tony Yip <tony@opensource.hk>
2017-03-06 10:07:10 +0800

    allow query string in fetch

--- src/fetch/url.js
+++ src/fetch/url.js
@@ -2,6 +2,6 @@
 
 module.exports = function (url) {
   const token = localStorage.getItem('api-token');
-  const uri = new URL(url, 'https://circleci.com/api/v1.1/');
-  return uri.toString() + '?circle-token=' + token;
+  const uri = new URL(url, 'https://circleci.com/api/v1.1/').toString();
+  return `${uri}${uri.indexOf('?') !== -1 ? '&' : '?'}circle-token=${token}`;
 };
